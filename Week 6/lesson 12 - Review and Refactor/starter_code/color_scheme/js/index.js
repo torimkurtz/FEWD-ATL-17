165fdcf04097f56c33819d2b65264b0d44996cdf
Tori <tori@github.com>
2018-02-04 23:31:17 -0500

    Lab

--- Week 6/lesson 12 - Review and Refactor/starter_code/color_scheme/js/index.js
+++ Week 6/lesson 12 - Review and Refactor/starter_code/color_scheme/js/index.js
@@ -1,22 +1,39 @@
-// Refactor the following code to instead use the 'this' keyword
+// // Refactor the following code to instead use the 'this' keyword
 
-$('#grayButton').click(switchGray);
-$('#whiteButton').click(switchWhite);
-$('#blueButton').click(switchBlue);
-$('#yellowButton').click(switchYellow);
+// $('#grayButton').click(switchGray);
+// $('#whiteButton').click(switchWhite);
+// $('#blueButton').click(switchBlue);
+// $('#yellowButton').click(switchYellow);
 
-function switchGray() {
-  $('body').attr('class', 'gray');
-}
+// function switchGray() {
+//   $('body').attr('class', 'gray');
+// }
 
-function switchWhite() {
-  $('body').attr('class', 'white');
-}
+// function switchWhite() {
+//   $('body').attr('class', 'white');
+// }
 
-function switchBlue() {
-  $('body').attr('class', 'blue');
-}
+// function switchBlue() {
+//   $('body').attr('class', 'blue');
+// }
 
-function switchYellow() {
-  $('body').attr('class', 'yellow');
-}
+// function switchYellow() {
+//   $('body').attr('class', 'yellow');
+// }
+
+
+
+$('li').click(function(){
+
+	var liColor = $(this).attr('id');
+	// console.log('hello');
+	$('body').attr('id', liColor);
+});
+
+// $(document).ready(function(){
+// 	$('li').click(function(){
+// 		var liColor = $(this).html());
+// 		// console.log('hello');
+// 		$('body').attr('id', liColor);
+// 	});
+// });
