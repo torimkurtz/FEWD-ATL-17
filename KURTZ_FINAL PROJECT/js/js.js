ec51ce004075255d1927999e77a542e555feac78
Tori <tori@github.com>
2018-02-25 15:21:02 -0500

    FINAL PROJECT SUMBIT

--- KURTZ_FINAL PROJECT/js/js.js
+++ KURTZ_FINAL PROJECT/js/js.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
 
-	var userFeed = new Instafeed({
-	get: 'user',
-	userId: '2428357482',
-	limit:12,
-	resolution: 'standard_resolution',
-	accessToken: '2428357482.1677ed0.5188b64eacaf443db5c3f8e20900b7b9',
-	sortBy: 'most-recent',
-	template: '<div class="col-lg-4 gallery instaimg"><a href="{{image}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
-	});
-	userFeed.run();
+	$( ".box" ).hover(function(event) {
+  		event.preventDefault();
+  		$(this).css("background-color", "hsla(314, 0%, 0%, 0.20)");
+  		}, function(){
+        $(this).css("background-color", "hsla(314, 0%, 35%, 0.29)");
 
-
-
-	$('.gallery').magnificPopup({
-	  type: 'image',
-	  delegate: 'a',
-	  gallery:{
-	    enabled:true
-	  }
 	});
 
+	$( ".box2" ).hover(function(event) {
+  		event.preventDefault();
+  		$(this).css("background-color", "hsla(314, 0%, 0%, 0.30)");
+  		}, function(){
+        $(this).css("background-color", "hsla(314, 0%, 35%, 0.29)");
+
+	});
+
+	$( ".box3" ).hover(function(event) {
+  		event.preventDefault();
+  		$(this).css("background-color", "hsla(314, 0%, 0%, 0.44)");
+  		}, function(){
+        $(this).css("background-color", "hsla(314, 0%, 35%, 0.29)");
+
+	});
 });
